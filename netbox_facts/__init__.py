ee40b1d01ee5dce75750e98f9d8f88c1e5e83969
Jonathan Senecal <jonathan.senecal@metrooptic.com>
2023-08-04 15:11:54 -0400

    Package changes

--- netbox_facts/__init__.py
+++ netbox_facts/__init__.py
@@ -1,19 +1,26 @@
 """Top-level package for NetBox Facts Plugin."""
 
 __author__ = """Jonathan Senecal"""
-__email__ = 'contact@jonathansenecal.com'
-__version__ = '0.0.1'
+__email__ = "contact@jonathansenecal.com"
+__version__ = "0.0.1"
 
 
 from extras.plugins import PluginConfig
+from django.conf import settings as django_settings
 
 
 class FactsConfig(PluginConfig):
-    name = 'netbox_facts'
-    verbose_name = 'NetBox Facts Plugin'
-    description = 'Gather operational facts from supported NetBox Devices'
-    version = 'version'
-    base_url = 'facts'
+    """Plugin configuration for the netbox_facts plugin."""
+
+    name = "netbox_facts"
+    verbose_name = "NetBox Facts Plugin"
+    description = "Gather operational facts from supported NetBox Devices"
+    version = __version__
+    base_url = "facts"
+    author = "Jonathan Senecal"
+    author_email = "contact@jonathansenecal.com"
+    default_settings = {}
 
 
 config = FactsConfig
+settings = django_settings.PLUGINS_CONFIG.get("netbox_facts", {})
